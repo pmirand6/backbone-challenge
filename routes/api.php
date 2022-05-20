6224ca989abe0b5313f7f80fa42cc1e0d9bae0fe
Pablo Miranda <pablo.miranda@waynimovil.com>
2022-05-20 17:28:48 -0300

    Improves defaults routes

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Backbone Challenge',
+        'author' => 'Pablo Miranda',
+        'github' => 'https://github.com/pmirand6',
+        'url' => 'https://github.com/pmirand6/backbone-challenge',
+    ]);
 });
-
 Route::get('/zip-codes/{zip_code}', GetZipCodesFileController::class);
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Backbone Challenge',
+        'author' => 'Pablo Miranda',
+        'github' => 'https://github.com/pmirand6',
+        'url' => 'https://github.com/pmirand6/backbone-challenge',
+    ]);
 });
